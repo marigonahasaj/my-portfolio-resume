c1f3f6418b1292fab6a40b4d0ece2d2b65bb6666
Marigona Hasaj <86122882+marigonahasaj@users.noreply.github.com>
2023-08-01 22:52:01 +0200

    fixes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -46,13 +46,13 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="lg:flex flex-col top-[35%] left-0 fixed z-10">
+    <div className="lg:flex flex-col top-3 left-0 fixed z-10">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li
             key={id}
             className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-transparent" +
+              "flex justify-between items-center w-40 h-12 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-transparent" +
               " " +
               style
             }
