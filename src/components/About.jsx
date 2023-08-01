30eab489b4431552c43979891a1f8fcdac267d4d
Marigona Hasaj <86122882+marigonahasaj@users.noreply.github.com>
2023-08-01 19:05:08 +0200

    new

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-100 bg-gradient-to-b from-gray-800 to-black text-white about"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-5">
+        My name is Marigona.  <br /> <br /> I am a mid Senior front end developer, designing, building and maintaining responsive webs and apps for over three years. Coming to the coding universe from a 7 year retail industry management experience, I gained proficiency in developing Enterprise Resource Development software. Customizing elements of the ERP software system, adjusting the user interface and usability, and updating the best practices guidelines.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+        I like to code matters from scratch and love the idea of bringing thoughts to life.  <br /> <br />
+        I value minimalistic designs, thoughtful branding of the content, and customer relatable experience. Let’s discover together how we can make your project convert better!<br /> <br />
+        I enjoy creating or redesigning a distinct identity for a product or service, get more traffic from search engine and social platforms.<br /> <br /> Eager to tackle more complex problems and continue to maximise user efficiency and experience, I shared work with product teams from concept to launch.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import siu from "../assets/portfolio/siu.png";
+import mvc from "../assets/portfolio/mvc.png";
+import dashcodeadmin from "../assets/portfolio/dashcodeadmin.png";
+import simonbrandt from "../assets/portfolio/simonbrandt.png";
+import geotech from "../assets/portfolio/geotech.png";
+import startup from "../assets/portfolio/startup.png";
+import bmw from "../assets/portfolio/bmw.png";
+import landingpg from "../assets/portfolio/landingpg.png";
+import geocad from "../assets/portfolio/geocad.png";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: siu,
+      link: 'https://siu.rks-gov.net/',
+    },
+    {
+      id: 2,
+      src: mvc,
+      link: 'https://store.myvirtualcart.com/',
+    },
+    {
+      id: 3,
+      src: geotech,
+      link: 'https://www.geotech.al/',
+    },
+    {
+      id: 4,
+      src: geocad,
+      link: 'https://geocad-ks.com/about',
+    },
+    {
+      id: 5,
+      src: simonbrandt,
+      link: 'https://simonbrandt.ch/',
+    },
+    {
+      id: 6,
+      src: dashcodeadmin,
+      link: 'https://dashcode.codeshaper.net/app/home',
+    },
+    {
+      id: 7,
+      src: startup,
+      link: 'https://startup-saas-modern.vercel.app/',
+    },
+    {
+      id: 8,
+      src: bmw,
+      link: 'https://www.bmw.de/de/home.html',
+    },
+    {
+      id: 9,
+      src: landingpg,
+      link: 'https://startup-online-course.vercel.app/',
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen portfolio"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, link, repo }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg" style={{maxHeight:'235px'}}>
+              <img
+                src={src}
+                alt="projects"
+                style={{height:'170px'}}
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-1 m-4 duration-200 hover:scale-105" onClick={ () => window.open(link, '_blank')}>
+                  Demo
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
